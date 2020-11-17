d83a39fd2d48cf9446f806476e5d592a68891a9c
UserTCQ <rayngu2005@gmail.com>
2020-11-17 20:58:40 +0700

    Initial commit

---
+++ FilePackerSharp/Packer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FilePackerSharp
+{
+    public class Packer : IDisposable
+    {
+        private FileStream[] _inputs;
+        private FileStream _output;
+
+        /// <summary>Get all files as array of FileStream for reading in top directory</summary>
+        public static FileStream[] GetFileStreams(string _directory)
+        {
+            string[] _filePaths = Directory.GetFiles(_directory, "*.*", SearchOption.TopDirectoryOnly);
+
+            FileStream[] _streams = new FileStream[_filePaths.Length];
+
+            for (int i = 0; i < _filePaths.Length; i++)
+                _streams[i] = File.OpenRead(_filePaths[i]);
+
+            return _streams;
+        }
+
+        /// <summary>
+        /// Pack multiple files into one data file
+        /// </summary>
+        public void PackFiles(FileStream[] _inputs, FileStream _output)
+        {
+            this._inputs = _inputs;
+            this._output = _output;
+
+            if (!_output.CanWrite)
+                throw new Exception("Output FileStream not writable!");
+
+            //list of bytes
+            List<byte> bytes = new List<byte>();
+
+            for (int i = 0; i < _inputs.Length; i++)
+            {
+                //checks
+                if (!_inputs[i].CanRead)
+                    throw new Exception($"Input FileStream at index {i} not readable!");
+
+                if (_inputs[i].Length > int.MaxValue)
+                    throw new Exception($"File larger than maximum allowed of 2GB!");
+
+                string _fileName = Path.GetFileName(_inputs[i].Name);
+
+                //write file name length
+                bytes.AddRange(BitConverter.GetBytes(_fileName.Length));
+
+                //write file name
+                bytes.AddRange(Encoding.UTF8.GetBytes(_fileName));
+
+                //write file size
+                bytes.AddRange(BitConverter.GetBytes((int)_inputs[i].Length));
+
+                //write file data
+                byte[] buffer = new byte[(int)_inputs[i].Length];
+                _inputs[i].Read(buffer, 0, buffer.Length);
+
+                bytes.AddRange(buffer);
+            }
+
+            //write to output stream
+            _output.Write(bytes.ToArray(), 0, bytes.Count);
+        }
+
+        //free unmanaged resources
+        public void Dispose()
+        {
+            foreach (var _input in _inputs)
+                _input.Dispose();
+
+            _output.Dispose();
+        }
+    }
+}
---
+++ FilePackerSharp/Unpacker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FilePackerSharp
+{
+    public class Unpacker : IDisposable
+    {
+        private FileStream _input;
+
+        /// <summary>
+        /// Unpack data file into multiple files
+        /// </summary>
+        public void UnpackFile(FileStream _input, string _directory)
+        {
+            this._input = _input;
+
+            while (_input.Position < _input.Length)
+            {
+                byte[] buffer;
+
+                //read file name length
+                buffer = new byte[4];
+                _input.Read(buffer, 0, 4);
+
+                int _fileNameLength = BitConverter.ToInt32(buffer, 0);
+
+                //read file name
+                buffer = new byte[_fileNameLength];
+                _input.Read(buffer, 0, _fileNameLength);
+
+                string _fileName = Encoding.UTF8.GetString(buffer);
+
+                //read file size
+                buffer = new byte[4];
+                _input.Read(buffer, 0, 4);
+
+                long _fileSize = BitConverter.ToInt32(buffer, 0);
+
+                //read file data
+                buffer = new byte[(int)_fileSize];
+                _input.Read(buffer, 0, (int)_fileSize);
+
+                //write to file
+                File.WriteAllBytes(_directory + $"/{_fileName}", buffer);
+            }
+        }
+
+        //free unmanaged memory
+        public void Dispose()
+        {
+            _input.Dispose();
+        }
+    }
+}
